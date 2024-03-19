7e688d781166b4286f3a50298b74875dfb80395e
Ranga1298 <37245334+Ranga1298@users.noreply.github.com>
2024-03-19 14:32:31 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ranga-bucket-new" # Replace with your S3 bucket name
+    bucket = "ra" # Replace with your S3 bucket name
     key    = "terraform.tfstate"
     region = "us-east-2"
 
